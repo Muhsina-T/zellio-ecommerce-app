e8acd8d371955c373ca2885f41c30769f6a4f6d7
Muhsina-T <muhsina2308@gmail.com>
2026-08-12 17:29:35 +0530

    error fixed while deploying

--- src/data/mobiles.ts
+++ src/data/mobiles.ts
@@ -1,57 +1,59 @@
 import type { Product } from "../types/Product";
 
-
 export const mobiles: Product[] = [
-{
-  _id: "1",
-  name: "iPhone 16 Pro",
-  brand: "Apple",
+  {
+    _id: "1",
+    name: "iPhone 16 Pro",
+    brand: "Apple",
 
-  image: "...",
-  images: ["...", "...", "..."],
+    image: "...",
+    images: ["...", "...", "..."],
 
-  storage: "256 GB",
-  color: "Black Titanium",
-  price: 129999,
+    storage: "256 GB",
+    color: "Black Titanium",
+    price: 129999,
+    costPrice: 110000,
 
-  rating: 5,
+    rating: 5,
 
-  description: "...",
+    description: "...",
 
-  stock: 20,
+    stock: 20,
 
-  reviews: [
-    {
-      id: 1,
-      user: "John",
-      comment: "Excellent phone",
-      rating: 5,
-    },
-  ],
+    reviews: [
+      {
+        id: 1,
+        user: "John",
+        comment: "Excellent phone",
+        rating: 5,
+      },
+    ],
 
-  variants: [
-    {
-      id: 1,
-      storage: "128 GB",
-      color: "Black Titanium",
-      price: 119999,
-      image: "...",
-    },
-    {
-      id: 2,
-      storage: "256 GB",
-      color: "Black Titanium",
-      price: 129999,
-      image: "...",
-    },
-    {
-      id: 3,
-      storage: "512 GB",
-      color: "White Titanium",
-      price: 149999,
-      image: "...",
-    },
-  ],
-}
-
+    variants: [
+      {
+        id: 1,
+        storage: "128 GB",
+        color: "Black Titanium",
+        price: 119999,
+        costPrice: 100000,
+        image: "...",
+      },
+      {
+        id: 2,
+        storage: "256 GB",
+        color: "Black Titanium",
+        price: 129999,
+        costPrice: 110000,
+        image: "...",
+      },
+      {
+        id: 3,
+        storage: "512 GB",
+        color: "White Titanium",
+        price: 149999,
+        costPrice: 125000,
+        image: "...",
+      },
+    ],
+  },
 ];
